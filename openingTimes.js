f8d2357c8294fd695d1e852f410b6a71bcfe6951
NetDesignr <michaiel.webdesigner@gmail.com>
2020-01-10 15:03:28 +0000

    Update content and logic

--- openingTimes.js
+++ openingTimes.js
@@ -10,31 +10,41 @@ var openingTimes = (function() {
     });
   }
 
-  function checkTime(start, end, slot) {
-    if (start < _getTime() && end >= _getTime()) {
-      var list = document.querySelector("#dsd-opening-times-slot").rows;
-      for (var i = 0; i < list.length; i++) {
-        var cells = list[i].cells;
-        for (var j = 0; j < cells.length; j++) {
-          if (cells[j].classList.contains(slot)) {
-            cells[j].classList.remove("invalid");
-            cells[j].classList.add("valid");
-          }
+  function _changeStatus(slot) {
+    var list = document.querySelector("#dsd-opening-times-slot").rows;
+    for (var i = 0; i < list.length; i++) {
+      var cells = list[i].cells;
+      for (var j = 0; j < cells.length; j++) {
+        if (cells[j].classList.contains(slot)) {
+          cells[j].classList.remove("invalid");
+          cells[j].classList.add("valid");
         }
       }
-    } else {
+    }
+  }
+
+  function checkTime(end, prevEnd, slot) {
+    if (_getTime() <= end && _getTime() > prevEnd) {
+      _changeStatus(slot);
+    }
+  }
+
+  function noSlots(lastSlot) {
+    if (_getTime() > lastSlot) {
       document.querySelector("h2.parchment").innerText =
-        "There are no slots available";
+        "Document ordering finished at 15:30 today";
     }
   }
 
   return {
-    checkTime: checkTime
+    checkTime: checkTime,
+    noSlots: noSlots
   };
 })();
 
-openingTimes.checkTime("09:45", "10:30", "slot1");
-openingTimes.checkTime("11:00", "11:45", "slot2");
-openingTimes.checkTime("12:15", "13:00", "slot3");
-openingTimes.checkTime("13:30", "14:15", "slot4");
-openingTimes.checkTime("14:45", "15:30", "slot5");
+openingTimes.checkTime("10:30", "07:00", "slot1");
+openingTimes.checkTime("11:45", "10:30", "slot2");
+openingTimes.checkTime("13:00", "11:45", "slot3");
+openingTimes.checkTime("14:15", "13:00", "slot4");
+openingTimes.checkTime("15:30", "14:15", "slot5");
+openingTimes.noSlots("15:30");
